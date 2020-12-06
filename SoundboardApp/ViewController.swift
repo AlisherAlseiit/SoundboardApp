b837a197c61d03689033a89f7855db7c25e118e9
Alisher <Alisher>
2020-12-06 16:11:45 +0600

    added voice, and now we can play it

--- SoundboardApp/ViewController.swift
+++ SoundboardApp/ViewController.swift
@@ -6,14 +6,88 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer:AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func ATapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "a", withExtension: "m4a")
+        
+        guard url != nil else {
+            return
+        }
+        
+        do {
+            
+        audioPlayer =  try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+
+            
+        }catch{
+            
+        }
+    }
+    
+    
+    
+    @IBAction func BTapped(_ sender: Any) {
+        
+        let url = Bundle.main.url(forResource: "B", withExtension: "m4a")
+        
+        guard url != nil else {
+            return
+        }
+           do {
+           audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+
+           }catch{
+               
+           }
+    }
+    
+    
+    @IBAction func CTapped(_ sender: Any) {
+        
+        let url = Bundle.main.url(forResource: "C", withExtension: "m4a")
+        
+        guard url != nil else {
+            return
+        }
+        
+           do {
+           audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+           }catch{
+               
+           }
+    }
+    
+    
+    @IBAction func DTapped(_ sender: Any) {
+        
+        let url = Bundle.main.url(forResource: "D", withExtension: "m4a")
+        
+        guard url != nil else {
+            return
+        }
+        
+           do {
+           audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+
+           }catch{
+               
+           }
+    }
+    
 }
 
